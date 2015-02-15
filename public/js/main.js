f7172b374209985a54297010d79c659b14d16e1a
Ofer Cohen <oc666@netvision.net.il>
2015-02-15 19:27:08 +0200

    improve UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,22 @@
-$(function() {
+$(function () {
 	$('.date').datetimepicker({
 		format: 'yyyy-MM-dd hh:mm:ss',
 	});
+
+	$('a[data-toggle="tab"]').on('shown.bs.tab', function () {
+		//save the latest tab; use cookies if you like 'em better:
+		localStorage.setItem('lastTab', $(this).attr('href'));
+	});
+
+	//go to the latest tab, if it exists:
+	var lastTab = localStorage.getItem('lastTab');
+	console.log(lastTab);
+	if (lastTab) {
+		$('a[href=' + lastTab + ']').tab('show');
+	} else {
+		// Set the first tab if cookie do not exist
+		$('a[data-toggle="tab"]:first').tab('show');
+	}
+
 });
+
